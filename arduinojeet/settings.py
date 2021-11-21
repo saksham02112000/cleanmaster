aba9ba62d7df68b3273abc729e0f400b2d70fe05
saksham02112000 <sakshamsrini@gmail.com>
2021-11-22 01:26:47 +0530

    database of server added

--- arduinojeet/settings.py
+++ arduinojeet/settings.py
@@ -76,12 +76,27 @@ WSGI_APPLICATION = 'arduinojeet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cleanmaster',
+        'USER': 'cleanmaster',
+        'PASSWORD': 'saksham',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
+# if DEBUG == True:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
 
 
 # Password validation
